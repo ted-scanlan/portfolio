5c202c5ec001a8b705ab49f027cf0b27d2480a4d
ted-scanlan <t.scanlan92@gmail.com>
2020-07-29 11:14:35 +0100

    Updates

---
+++ precache-manifest.c7baf5ddb14d34b677734de80c7d9e49.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dba59d627b58a9ef99a40bd7bad93074",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d18be9cb1081ca6d4f75",
+    "url": "/portfolio/static/css/main.af4ae01b.chunk.css"
+  },
+  {
+    "revision": "a4624c5cb32e9717db0c",
+    "url": "/portfolio/static/js/2.f5722a60.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.f5722a60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d18be9cb1081ca6d4f75",
+    "url": "/portfolio/static/js/main.8466510b.chunk.js"
+  },
+  {
+    "revision": "7406c4497b9cca9bbe46",
+    "url": "/portfolio/static/js/runtime-main.d108c971.js"
+  },
+  {
+    "revision": "0883074a392e93ba3f4fa5f27698c383",
+    "url": "/portfolio/static/media/algorithms-medium-large.0883074a.png"
+  },
+  {
+    "revision": "0037ea9b88dd79a6bed55279aeff39e1",
+    "url": "/portfolio/static/media/clearscore-medium-large.0037ea9b.png"
+  },
+  {
+    "revision": "fdd393b92ec01cf19820a7c50716dfc1",
+    "url": "/portfolio/static/media/clearscore-medium-new.fdd393b9.png"
+  },
+  {
+    "revision": "45e5a696f37eb7cea3fb6980fc632ea0",
+    "url": "/portfolio/static/media/clearscore-twenty-twenty.45e5a696.png"
+  },
+  {
+    "revision": "e54947b0acb46646017c495c03a7585c",
+    "url": "/portfolio/static/media/screenshot-01-post-thumbnail-large.e54947b0.png"
+  },
+  {
+    "revision": "dfa451cbb4a883da3f10235b15ec6a0a",
+    "url": "/portfolio/static/media/set-alarm-large.dfa451cb.gif"
+  },
+  {
+    "revision": "a1f1a307d33e2f6487f495c1087b5e4c",
+    "url": "/portfolio/static/media/strawberry-medium.a1f1a307.gif"
+  }
+]);
